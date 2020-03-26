9a762b7096cca98ebbbd74fcd903c5e8e296cfac
michelle19l <2675625405@qq.com>
2020-03-27 00:12:32 +0800

    Site updated: 2020-03-27 00:12:27

--- new_image_path.py
+++ new_image_path.py
@@ -15,11 +15,11 @@ def replace_1():
 	#input_str=input("原图片路径") # eg. E:\study\大二下\漏洞测试\作业\作业一\
 	#output_str='/images'+input("现图片路径") # eg. /漏洞测试作业一/,若在根目录下则输入/
 
-	input_str="E:\\blog\hexo\source\_posts\\".split(" ")
-	output_str="/images/漏测作业二/"
+	input_str="E:\blog\hexo\source\_posts\\".split(" ")
+	output_str="/images/数据库上机L3/"
 
 	style="zoom：50%;"
-	input_file='E:\\blog\hexo\source\_posts\漏测作业二.md'
+	input_file='E:\\blog\hexo\source\_posts\数据库上机L3.md'
 	output_file="E:\\blog\hexo\source\_posts\output.txt"
 
 	file_data = ""
